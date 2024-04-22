27bfb0d7d8bcdbf3bef227dd8e6652d41a85763b
Michael  Xavier Lie <mlie0008@student.monash.edu>
2024-04-23 00:37:18 +1000

    Only show address fields when appropriate delivery method is chosen

--- templates/Orders/add.php
+++ templates/Orders/add.php
@@ -20,42 +20,37 @@
                 <legend><?= __('Add Order') ?></legend>
                 <?php
                 echo $this->Form->hidden('status', ['value' => 'Pending']);
-                echo $this->Form->control('delivery_method', ['options' => ['Pick-up' => 'Pick-up', 'Delivery' => 'Delivery']]);
-                echo $this->Form->control('address');
-                echo $this->Form->control('suburb');
-                echo $this->Form->control('state', ['options' => ['ACT' => 'ACT', 'NSW' => 'NSW', 'NT' => 'NT','QLD' => 'QLD', 'SA' => 'SA', 'TAS' =>'TAS', 'VIC' => 'VIC', 'WA' => 'WA']]);
-                echo $this->Form->control('postcode');
+                echo $this->Form->control('delivery_method', [
+                    'options' => ['Pick-up' => 'Pick-up', 'Delivery' => 'Delivery'],
+                    'onchange' => 'toggleAddressVisibility(this.value)',
+                    'label' => ['text' => 'Delivery Method *', 'escape' => false]
+                ]);
+
+                ?>
+                <div id="addressFields" style="display: none;">
+                    <?php
+                    echo $this->Form->control('address');
+                    echo $this->Form->control('suburb');
+                    echo $this->Form->control('state', ['options' => ['ACT' => 'ACT', 'NSW' => 'NSW', 'NT' => 'NT','QLD' => 'QLD', 'SA' => 'SA', 'TAS' =>'TAS', 'VIC' => 'VIC', 'WA' => 'WA']]);
+                    echo $this->Form->control('postcode');
+                    ?>
+                </div>
+                <script>
+                    function toggleAddressVisibility(deliveryMethod) {
+                        var displayStyle = deliveryMethod === 'Delivery' ? 'block' : 'none';
+                        document.getElementById('addressFields').style.display = displayStyle;
+                    }
+                </script>
+                <?php
                 echo $this->Form->control('note');
                 echo $this->Form->hidden('subtotal', ['value' => '0.00']);
                 echo $this->Form->hidden('delivery_fee', ['value' => '0.00']);
                 echo $this->Form->control('user_id', ['options' => $users]);
                 ?>
-                <legend><?= __('Select Menu Items:') ?></legend>
+                <legend><?= __('Select Menu Items: *') ?></legend>
                 <?php
                     echo $this->Form->control('items._ids', ['options' => $items, 'multiple' => 'checkbox', 'label' => false]);
                 ?>
-<!--                <div class="item-selection">
-                    <?php /*foreach ($items as $itemId => $itemName) : */?>
-                        <div class="item-row">
-                            <div class="item-checkbox">
-                                <?php /*= $this->Form->control('items._ids', [
-                                    'type' => 'checkbox',
-                                    'value' => $itemId,
-                                    'label' => $itemName
-                                ]) */?>
-                            </div>
-                            <div class="item-quantity">
-                                <?php /*= $this->Form->control('items.' . $itemId . '.quantity', [
-                                    'type' => 'number',
-                                    'label' => false,
-                                    'class' => 'quantity-input',
-                                    'min' => 1,
-                                    'step' => 1
-                                ]) */?>
-                            </div>
-                        </div>
-                    <?php /*endforeach; */?>
-                </div>-->
             </fieldset>
             <?= $this->Form->button(__('Submit')) ?>
             <?= $this->Form->end() ?>
