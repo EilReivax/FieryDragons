cb4447cbd71420b4ca6d30767098c9c9578667cf
gbman <chun.h.man@gmail.com>
2024-05-08 02:57:28 +1000

    reverting captcha changes

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -198,26 +198,24 @@ class AuthController extends AppController
     {
         $this->request->allowMethod(['get', 'post']);
         if($this->request->is('post')){
-            if($this->Recaptcha->verify()){
-                $result = $this->Authentication->getResult();
+            $result = $this->Authentication->getResult();
 
                 // if user passes authentication, grant access to the system
-                if ($result && $result->isValid()) {
-                    // set a fallback location in case user logged in without triggering 'unauthenticatedRedirect'
-                    $fallbackLocation = ['controller' => 'Pages', 'action' => 'index'];
+            if ($result && $result->isValid()) {
+                // set a fallback location in case user logged in without triggering 'unauthenticatedRedirect'
+                $fallbackLocation = ['controller' => 'Pages', 'action' => 'index'];
 
                     // and redirect user to the location they're trying to access
-                    return $this->redirect($this->Authentication->getLoginRedirect() ?? $fallbackLocation);
-                }
+                return $this->redirect($this->Authentication->getLoginRedirect() ?? $fallbackLocation);
+            }
 
                 // display error if user submitted their credentials but authentication failed
-                if ($this->request->is('post') && !$result->isValid()) {
-                    $this->Flash->error('Email address and/or Password is incorrect. Please try again. ');
-                }
+            if ($this->request->is('post') && !$result->isValid()) {
+                $this->Flash->error('Email address and/or Password is incorrect. Please try again. ');
             }
         }
-
     }
+
 
 
     /**
