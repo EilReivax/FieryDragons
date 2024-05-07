2364e7841ef78dcd02729de72892675003ef1289
gbman <chun.h.man@gmail.com>
2024-05-08 02:11:44 +1000

    add recaptcha trial 1.1

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -210,21 +210,23 @@ class AuthController extends AppController
                 $this->Flash->error('reCAPTCHA verification failed. Please try again.');
                 return $this->redirect(['action' => 'submitForm']);
             }
-        $result = $this->Authentication->getResult();
+            $result = $this->Authentication->getResult();
 
-        // if user passes authentication, grant access to the system
-        if ($result && $result->isValid()) {
-            // set a fallback location in case user logged in without triggering 'unauthenticatedRedirect'
-            $fallbackLocation = ['controller' => 'Pages', 'action' => 'index'];
+            // if user passes authentication, grant access to the system
+            if ($result && $result->isValid()) {
+                // set a fallback location in case user logged in without triggering 'unauthenticatedRedirect'
+                $fallbackLocation = ['controller' => 'Pages', 'action' => 'index'];
 
-            // and redirect user to the location they're trying to access
-            return $this->redirect($this->Authentication->getLoginRedirect() ?? $fallbackLocation);
+                // and redirect user to the location they're trying to access
+                return $this->redirect($this->Authentication->getLoginRedirect() ?? $fallbackLocation);
+            }
+
+            // display error if user submitted their credentials but authentication failed
+            if ($this->request->is('post') && !$result->isValid()) {
+                $this->Flash->error('Email address and/or Password is incorrect. Please try again. ');
+            }
         }
 
-        // display error if user submitted their credentials but authentication failed
-        if ($this->request->is('post') && !$result->isValid()) {
-            $this->Flash->error('Email address and/or Password is incorrect. Please try again. ');
-        }
     }
 
     /**
