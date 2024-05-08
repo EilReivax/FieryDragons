a7325bb62200ba9446b3d587fb8abe7feb754977
Michael  Xavier Lie <mlie0008@student.monash.edu>
2024-05-08 12:42:54 +1000

    Added authorisation policies for item, order, payment, and user

---
+++ src/Policy/OrderPolicy.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Policy;
+
+use App\Model\Entity\Order;
+use Authorization\IdentityInterface;
+
+/**
+ * Order policy
+ */
+class OrderPolicy
+{
+    /**
+     * Check if $user can add Order
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Order $order
+     * @return bool
+     */
+    public function canAdd(IdentityInterface $user, Order $order)
+    {
+    }
+
+    /**
+     * Check if $user can edit Order
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Order $order
+     * @return bool
+     */
+    public function canEdit(IdentityInterface $user, Order $order)
+    {
+    }
+
+    /**
+     * Check if $user can delete Order
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Order $order
+     * @return bool
+     */
+    public function canDelete(IdentityInterface $user, Order $order)
+    {
+    }
+
+    /**
+     * Check if $user can view Order
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Order $order
+     * @return bool
+     */
+    public function canView(IdentityInterface $user, Order $order)
+    {
+    }
+}
