d0ee2bade887fb05c169a324561772b256f3733e
Michael  Xavier Lie <mlie0008@student.monash.edu>
2024-05-08 22:33:10 +1000

    Changed edit form to only display relevant elements

--- templates/Orders/edit.php
+++ templates/Orders/edit.php
@@ -24,16 +24,28 @@
             <fieldset>
                 <legend><?= __('Edit Order') ?></legend>
                 <?php
-                echo $this->Form->hidden('status', ['value' => 'Pending']);
-                echo $this->Form->control('delivery_method', ['options' => ['Pick-up' => 'Pick-up', 'Delivery' => 'Delivery']]);
-                echo $this->Form->control('address');
-                echo $this->Form->control('suburb');
-                echo $this->Form->control('state', ['options' => ['ACT' => 'ACT', 'NSW' => 'NSW', 'NT' => 'NT','QLD' => 'QLD', 'SA' => 'SA', 'TAS' =>'TAS', 'VIC' => 'VIC', 'WA' => 'WA']]);
-                echo $this->Form->control('postcode');
+                echo $this->Form->control('delivery_method', [
+                    'options' => ['Pick-up' => 'Pick-up', 'Delivery' => 'Delivery'],
+                    'onchange' => 'toggleAddressVisibility(this.value)',
+                    'label' => ['text' => 'Delivery Method *', 'escape' => false]
+                ]);
+
+                ?>
+                <div id="addressFields" style="display: none;">
+                    <?php
+                    echo $this->Form->control('address');
+                    echo $this->Form->control('suburb');
+                    echo $this->Form->control('state', ['options' => ['ACT' => 'ACT', 'NSW' => 'NSW', 'NT' => 'NT','QLD' => 'QLD', 'SA' => 'SA', 'TAS' =>'TAS', 'VIC' => 'VIC', 'WA' => 'WA']]);
+                    echo $this->Form->control('postcode');
+                    ?>
+                </div>
+                <script>
+                    function toggleAddressVisibility(deliveryMethod) {
+                        document.getElementById('addressFields').style.display = deliveryMethod === 'Delivery' ? 'block' : 'none';
+                    }
+                </script>
+                <?php
                 echo $this->Form->control('note');
-                echo $this->Form->hidden('subtotal', ['value' => '0.00']);
-                echo $this->Form->hidden('delivery_fee', ['value' => '0.00']);
-                echo $this->Form->control('user_id', ['options' => $users]);
                 ?>
                 <legend><?= __('Select Menu Items:') ?></legend>
                 <?php
