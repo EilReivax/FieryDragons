945589dc8b0e8b70ead3b0bca300b5dc3a2aad49
gbman <chun.h.man@gmail.com>
2024-05-22 22:19:51 +1000

    fix nonce expiry

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -76,7 +76,7 @@ class AuthController extends AppController
             if ($user) {
                 // Set nonce and expiry date
                 $user->nonce = Security::randomString(128);
-                $user->nonce_expiry = new DateTime('24 hours');
+                $user->nonce_expiry = new DateTime('1 hours');
                 if ($this->Users->save($user)) {
                     // Now let's send the password reset email
                     $mailer = new Mailer('default');
